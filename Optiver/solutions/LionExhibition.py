362c44409af7cad0f169277a22c92e534a26145c
Nam Tran <nam.tran2@gmail.com>
2024-07-24 21:32:55 -0700

    update OA solution

--- Optiver/solutions/LionExhibition.py
+++ Optiver/solutions/LionExhibition.py
@@ -1,62 +1,82 @@
-import heapq
 from collections import defaultdict
+from heapq import heappush, heappop
 
 
-class LionCompetition:
+class LionExhibition:
     def __init__(self, lions, schedule):
-        self.our_lions = {}  # To store our lions' heights by their names
-        self.schedule = {}  # To store entry and exit times by lion names
-        for lion in lions:
-            name, height = lion[0], lion[1]
-            self.our_lions[name] = height
+        # Map of our lions' names to their sizes
+        self.my_lions = {lion: size for lion, size in lions}
+        # Set of our lions that are present at any given time
+        self.my_lions_present = set()
 
-        for entry in schedule:
-            name, enter_time, exit_time = entry[0], entry[1], entry[2]
-            self.schedule[name] = (enter_time, exit_time)
+        # Ignore map to keep track of our lions entering and leaving
+        self.ignore_map = defaultdict(int)
 
-        self.in_room_lions = defaultdict(int)  # A dictionary to track counts of lions in the room by height
-        self.our_lions_in_room = set()  # A set to track our lions in the room
-        self.max_heap = []  # A max-heap (implemented as a min-heap with negative heights)
+        # Other lions that are present (max heap by size)
+        self.max_height = []
+        # Lions that have left but are still in the heap (lazy deletion)
+        self.other_lions_left = defaultdict(int)
 
-    def lion_entered(self, current_time, height):
-        # This will be used for external lions
-        self.in_room_lions[height] += 1
-        heapq.heappush(self.max_heap, -height)  # Push the negative height to simulate a max-heap
+        # Schedule sorted by time of arrival/leaving
+        self.schedule = []
+        for name, arrival, departure in schedule:
+            self.schedule.append((arrival, name, 'arrive'))
+            self.schedule.append((departure, name, 'depart'))
+            self.ignore_map[(self.my_lions[name], arrival, 'arrive')] += 1
+            self.ignore_map[(self.my_lions[name], departure, 'depart')] += 1
 
-    def lion_left(self, current_time, height):
-        # Decrease the count of lions with the given height
-        self.in_room_lions[height] -= 1
-        if self.in_room_lions[height] == 0:
-            del self.in_room_lions[height]
+        self.schedule.sort(key=lambda event: event[0])
+        # Schedule index
+        self.schedule_index = 0
 
-        # We need to maintain the max-heap to reflect the current maximum lion height
-        # Lazy removal: just mark it as invalid and clean up during inspection
-        while self.max_heap and -self.max_heap[0] not in self.in_room_lions:
-            heapq.heappop(self.max_heap)
-
-    def get_biggest_lions(self, current_time):
-        if self.max_heap:
-            max_competing_height = -self.max_heap[0]
+    def lion_entered(self, time, height):
+        key = (height, time, 'arrive')
+        if self.ignore_map[key] > 0:
+            self.ignore_map[key] -= 1
         else:
-            max_competing_height = 0
+            heappush(self.max_height, -height)
 
-        biggest_lions = [name for name in self.our_lions_in_room if self.our_lions[name] >= max_competing_height]
-        return sorted(biggest_lions)
+    def lion_left(self, time, height):
+        key = (height, time, 'depart')
+        if self.ignore_map[key] > 0:
+            self.ignore_map[key] -= 1
+        else:
+            self.other_lions_left[height] += 1
+
+    def get_biggest_lions(self, time):
+        # Flush our lions to the ones currently in the room based on time
+        while self.schedule_index < len(self.schedule) and self.schedule[self.schedule_index][0] <= time:
+            event_time, name, event_type = self.schedule[self.schedule_index]
+            if event_type == 'arrive':
+                self.my_lions_present.add((name, self.my_lions[name]))
+            else:
+                self.my_lions_present.remove((name, self.my_lions[name]))
+            self.schedule_index += 1
+
+        # Flush the heap of other lions to find the tallest and still present
+        while self.max_height:
+            tallest = -self.max_height[0]
+            if self.other_lions_left[tallest] > 0:
+                heappop(self.max_height)
+                self.other_lions_left[tallest] -= 1
+            else:
+                break
+
+        tallest = -self.max_height[0] if self.max_height else 0
+        candidates = [name for name, height in self.my_lions_present if height >= tallest]
+        candidates.sort()
+        return candidates
 
 
-sol = LionCompetition([("marry", 300), ("rob", 250)], [("marry", 10, 15), ("rob", 13, 20)])
+sol = LionExhibition([("marry", 300), ("rob", 250)], [("marry", 10, 15), ("rob", 13, 20)])
 sol.lion_entered(8, 200)
 sol.lion_entered(10, 310)
 sol.lion_entered(10, 300)
-
+print(sol.get_biggest_lions(11))
 sol.lion_entered(13, 250)
 sol.lion_left(13, 310)
-print(sol.get_biggest_lions(11))
-print(sol.in_room_lions)
-print(sol.our_lions_in_room)
-print(sol.max_heap)
 print(sol.get_biggest_lions(13))
-# sol.lion_left(15, 300)
-# print(sol.get_biggest_lions(16))
-# sol.lion_left(16, 310)
-# sol.lion_left(20, 310)
+sol.lion_left(15, 300)
+print(sol.get_biggest_lions(16))
+sol.lion_left(16, 310)
+sol.lion_left(20, 310)
