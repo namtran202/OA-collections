45f65ee174b2ff9b36242cd6856d82d35dd4c5fb
Nam Tran <nam.tran2@gmail.com>
2024-07-24 23:49:42 -0700

    add OA questions and solutions

--- Optiver/solutions/FuturePricingEngine.py
+++ Optiver/solutions/FuturePricingEngine.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-import bisect
 
 
 @dataclass
@@ -19,11 +18,11 @@ class FuturePricingEngine:
         self.dividends[dividend_id - 1 - self.pass_dividends] = updated_dividend
 
     def calculate_future_price(self, days_to_future: int) -> int:
-        if self.current_day < days_to_future:
+        if self.current_day >= days_to_future:
             return self.stock_price
 
         i = 0
-        while i < len(self.dividends) and self.dividends[i].days >= days_to_future:
+        while i < len(self.dividends) and self.dividends[i].days <= days_to_future:
             self.stock_price -= self.dividends[i].amount
             self.current_day = self.dividends[i].days
             i += 1
@@ -37,3 +36,11 @@ print(engine.calculate_future_price(15))
 engine.update_dividend(2, Dividend(40, 20))
 print(engine.calculate_future_price(15))
 print(engine.calculate_future_price(25))
+
+print("--------")
+engine2 = FuturePricingEngine(100, [Dividend(1, 1), Dividend(2, 2), Dividend(3, 3), Dividend(4, 4), Dividend(5, 5)])
+print(engine2.calculate_future_price(1))
+print(engine2.calculate_future_price(2))
+print(engine2.calculate_future_price(3))
+print(engine2.calculate_future_price(4))
+print(engine2.calculate_future_price(5))
